2873ae19afe4212758923ac5d7d89c773f24e10b
notdeltaxd <sayankabiraj2018@gmail.com>
2025-11-23 20:54:03 +0530

    fix: Fix vercel build issues

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,4 @@
 import { handle } from '@hono/node-server/vercel'
-import app from '../src/server.js'
-import { serve } from '@hono/node-server'
-
-export const config = {
-    api: {
-        bodyParser: false
-    }
-}
+import app from '../dist/server.js'
 
 export default handle(app)
-
-if (!process.env.VERCEL) {
-    const port = process.env.PORT || 3000
-    serve(
-        {
-            fetch: app.fetch,
-            port: Number(port)
-        },
-        (info) => {
-            console.log(`Server running at http://localhost:${info.port}`)
-        }
-    )
-}
